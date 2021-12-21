5bf29aedf5f14d4b5fb915842b01a774e18209f0
Bob German <bgerman1@hotmail.com>
2021-12-21 13:28:02 -0500

    Now shows order numbers

--- 01-Start/client/modules/index.js
+++ 01-Start/client/modules/index.js
@@ -20,15 +20,33 @@ async function getUserProfile(clientSideToken) {
 
 async function getOrdersForEmployee(employeeId)
 {
-    
+    const response = await fetch ("/ordersForEmployee?employeeId=2", {
+        "method": "get",
+        "headers": {
+            "content-type": "application/json"
+        },
+        "cache": "default"
+    });
+    if (response.ok) {
+        const orders = await response.json();
+        return orders.value;
+    } else {
+        const error = await response.json();
+        console.log (`ERROR: ${error}`);
+        throw (error);
+    }    
 }
 
 async function displayUI() {
 
     const displayElement = document.getElementById('content');
     const imageElement = document.getElementById('image');
+    const ordersElement = document.getElementById('orders');
+
     try {
         const userProfile = await getUserProfile();
+        const orders = await getOrdersForEmployee(2);
+
 
         displayElement.innerHTML = `
             <h1>Hello ${userProfile.displayName}</h1>
@@ -37,6 +55,12 @@ async function displayUI() {
             Job Title: ${userProfile.jobTitle}<br />
         `;
         imageElement.src = `data:image/bmp;base64,${userProfile.photo}`;
+
+        orders.forEach(order => {
+            const orderElement = document.createElement('p');
+            orderElement.innerText = order.OrderID;
+            ordersElement.appendChild(orderElement);
+        });
     }
     catch (error) {            // If here, we had some other error
             displayElement.innerText = `Error: ${JSON.stringify(error)}`;
