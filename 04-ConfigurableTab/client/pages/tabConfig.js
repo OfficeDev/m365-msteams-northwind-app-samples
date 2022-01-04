947f9ea364f4b7f5ac10f6d6be15df587a0dabc1
Bob German <bgerman1@hotmail.com>
2022-01-04 17:23:50 -0500

    Now displays options and sets validity state

--- 04-ConfigurableTab/client/pages/tabConfig.js
+++ 04-ConfigurableTab/client/pages/tabConfig.js
@@ -1,3 +1,4 @@
+import 'https://statics.teams.cdn.office.net/sdk/v1.11.0/js/MicrosoftTeams.min.js';
 // import {
 //     getLoggedInEmployee
 // } from '../northwindIdentity/identityService.js';
@@ -12,21 +13,26 @@ async function displayUI() {
     const messageDiv = document.getElementById('message');
 
     try {
-        const employee = 1; // await getLoggedInEmployee();
-        if (employee) {
-            
-            const categories = await getCategories();
-            categories.forEach((category) => {
-                const option = document.createElement('option');
-                option.value = category.categoryId;
-                option.innerText = category.displayName;
-                categorySelect.appendChild(option);
-            });
+        microsoftTeams.initialize(async () => {
 
-        }
+            const employee = 1; // await getLoggedInEmployee();
+            if (employee) {
+
+                const categories = await getCategories();
+                categories.forEach((category) => {
+                    const option = document.createElement('option');
+                    option.value = category.categoryId;
+                    option.innerText = category.displayName;
+                    categorySelect.appendChild(option);
+                });
+                categorySelect.addEventListener('change', () => {
+                    microsoftTeams.settings.setValidityState(true);
+                });
+            }
+        });
     }
     catch (error) {            // If here, we had some other error
-        messageDiv.innerText = `Error: ${JSON.stringify(error)}`;
+        messageDiv.innerText = `Error: ${JSON.stringify(error.message)}`;
     }
 }
 
