fa051483baa2b902bace7277306b2813e3060047
Bob German <bgerman1@hotmail.com>
2022-02-07 16:03:21 -0500

    Fix extra nav, teams API init bug

--- A03-TeamsSSO/client/components/navigation.js
+++ A03-TeamsSSO/client/components/navigation.js
@@ -1,5 +1,7 @@
+import { inTeams } from '../modules/teamsHelpers.js';
+
 const topNavLinks = [
-    { "text": "Home", "url": "/index.html"},
+    { "text": "Home", "url": "/index.html" },
     { "text": "My Orders", "url": "/pages/myOrders.html" },
     { "text": "Products", "url": "/pages/categories.html" },
 ];
@@ -8,17 +10,19 @@ export class topNavPanel extends HTMLElement {
 
     async connectedCallback() {
 
-        let listItemHtml = "";
-        topNavLinks.forEach(link => {
-            if (window.location.href.indexOf(link.url) < 0) {
-                listItemHtml += '<li><a href="' + link.url + '">' + link.text + '</a></li>';
-            } else {
-                return listItemHtml += '<li><a href="' + link.url + '" class="selected">' + link.text + '</a></li>';
-            }
-        });
-        this.innerHTML = `
-            <ul class="topnav">${listItemHtml}</ul>
-        `;
+        if (!(await inTeams())) {
+            let listItemHtml = "";
+            topNavLinks.forEach(link => {
+                if (window.location.href.indexOf(link.url) < 0) {
+                    listItemHtml += '<li><a href="' + link.url + '">' + link.text + '</a></li>';
+                } else {
+                    return listItemHtml += '<li><a href="' + link.url + '" class="selected">' + link.text + '</a></li>';
+                }
+            });
+            this.innerHTML = `
+                <ul class="topnav">${listItemHtml}</ul>
+            `;
+        }
 
     }
 }
