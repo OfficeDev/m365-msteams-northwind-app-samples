50b2b647bd01e365eca7d45e6149351d12f5a6cd
rabwill <rabiawilliams@gmail.com>
2022-03-22 14:01:51 +1000

    Check app ID issue

--- src/extend-with-capabilities/aad/Deeplink/client/pages/orderDetail.js
+++ src/extend-with-capabilities/aad/Deeplink/client/pages/orderDetail.js
@@ -2,20 +2,24 @@ import {
     getOrder
 } from '../modules/northwindDataService.js';
 import 'https://statics.teams.cdn.office.net/sdk/v1.11.0/js/MicrosoftTeams.min.js';
+let order={};
 async function displayUI() {
 
     const displayElement = document.getElementById('content');
     const detailsElement = document.getElementById('orderDetails');
     const copyUrlElement = document.getElementById('btnCopyOrderUrl');
     const copyMsgElement=document.getElementById('copyMessage');
+    const copySectionElement=document.getElementById('copySection');
     try {
 
         const searchParams = new URLSearchParams(window.location.search);
         microsoftTeams.initialize(async () => {
-        if (searchParams.has('orderId')) {
-            const orderId = searchParams.get('orderId');
+        microsoftTeams.getContext(async function (context) {
+     
+        if (searchParams.has('orderId')||context.subEntityId) {
+            const orderId = searchParams.get('orderId')?searchParams.get('orderId'):context.subEntityId;
 
-            const order = await getOrder(orderId);
+            order = await getOrder(orderId);
 
             displayElement.innerHTML = `
                     <h1>Order ${order.orderId}</h1>
@@ -37,30 +41,33 @@ async function displayUI() {
                 detailsElement.append(orderRow);
 
             });
-            copyUrlElement.addEventListener('click',  ev => {
-                try {
-                    microsoftTeams.getContext(function (context) { 
-                        console.log(context)
-                        //entityId
-                    var textarea = document.createElement("textarea");
-                    var encodedWebUrl = encodeURI(`https://${window.location.hostname}/pages/orderDetail.html?orderId=${orderId}`);
-                    const encodedContext = encodeURI(`{"subEntityId": "${orderId}"}`);
-                    const deeplink = `https://teams.microsoft.com/l/entity/3d79f44f-c85e-4638-a49c-33b0033a2721/${context.entityId}?webUrl=${encodedWebUrl}&context=${encodedContext}`;
+            if(searchParams.has('orderId')){
+                copyUrlElement.addEventListener('click',  ev => {
+                    try { 
+                        var textarea = document.createElement("textarea");
+                        const encodedContext = encodeURI(`{"subEntityId": "${order.orderId}"}`);
+                        const deeplink = `https://teams.microsoft.com/l/entity/5135eda5-f7a4-4999-9c4d-b81ca00e2c43/OrderDetails?&context=${encodedContext}`;
+                        textarea.value = deeplink;
+                        document.body.appendChild(textarea);
+                        textarea.select();
+                        document.execCommand("copy"); //deprecated but there is an issue with navigator.clipboard api
+                        document.body.removeChild(textarea); 
+                        copyMsgElement.innerHTML="Link copied!"
+                    
+                    } catch (err) {
+                        console.error('Failed to copy: ', err);
+                      }});
+            }else{
+                copySectionElement.style.display = "none";
                
-                    textarea.value = deeplink;
-                    document.body.appendChild(textarea);
-                    textarea.select();
-                    document.execCommand("copy"); //deprecated but there is an issue with navigator.clipboard api
-                    document.body.removeChild(textarea); 
-                    copyMsgElement.innerHTML="Link copied!"
-                    });
-                } catch (err) {
-                    console.error('Failed to copy: ', err);
-                  }
-            });
+            }
+            
+        
+    
 
         }
     });
+});
        
     }
     catch (error) {            // If here, we had some other error
