a2b3c2acb1602b1e62b8ddbc0f6501964e11b1dd
rabwill <rabiawilliams@gmail.com>
2022-04-08 00:43:08 +1000

    Added context based themeing

--- src/extend-with-capabilities/ExtendTeamsforM365/client/modules/teamsHelpers.js
+++ src/extend-with-capabilities/ExtendTeamsforM365/client/modules/teamsHelpers.js
@@ -1,5 +1,4 @@
-import 'https://statics.teams.cdn.office.net/sdk/2.0.0-beta.1/js/MicrosoftTeams.min.js';
-
+import 'https://res.cdn.office.net/teams-js/2.0.0-beta.1/js/MicrosoftTeams.min.js';
 // async function returns true if we're running in Teams
 export async function inTeams() {
     // There is no supported way to do this in the Teams JavaScript SDK v1.
@@ -17,14 +16,48 @@ function setTheme (theme) {
     const el = document.documentElement;
     el.setAttribute('data-theme', theme); // switching CSS
 };
-  
-microsoftTeams.initialize(() => {
-    // Set the CSS to reflect the current Teams theme
-    microsoftTeams.getContext((context) => {
-        setTheme(context.theme);
-    });
-    // When the theme changes, update the CSS again
-    microsoftTeams.registerOnThemeChangeHandler((theme) => {
-        setTheme(theme);
-    });
-});
+if(microsoftTeams.app !== undefined) {
+  microsoftTeams.app.initialize();
+  microsoftTeams.app.getContext().then(context=> { 
+    if(context) {
+      setTheme(context.theme);     
+      switch(context.app.host.name){
+        case "Teams":{
+          let element = document.createElement("link");
+          element.setAttribute("rel", "stylesheet");
+          element.setAttribute("type", "text/css");
+          element.setAttribute("href", "/northwind.css");
+          document.getElementsByTagName("head")[0].appendChild(element);
+          // When the theme changes, update the CSS again: Only for teams
+          microsoftTeams.app.registerOnThemeChangeHandler((theme) => {
+          setTheme(theme);
+        });
+        };
+        
+        break;
+        case "Outlook":{
+          let element = document.createElement("link");
+          element.setAttribute("rel", "stylesheet");
+          element.setAttribute("type", "text/css");
+          element.setAttribute("href", "/northwind-outlook.css");
+          document.getElementsByTagName("head")[0].appendChild(element);
+        };
+          break;
+        case "Office":{
+          let element = document.createElement("link");
+          element.setAttribute("rel", "stylesheet");
+          element.setAttribute("type", "text/css");
+          element.setAttribute("href", "/northwind-office.css");
+          document.getElementsByTagName("head")[0].appendChild(element);
+        }
+        break;
+        default:{
+          let element = document.createElement("link");
+          element.setAttribute("rel", "stylesheet");
+          element.setAttribute("type", "text/css");
+          element.setAttribute("href", "/northwind.css");
+          document.getElementsByTagName("head")[0].appendChild(element);
+        }
+      }     
+    }});
+}
