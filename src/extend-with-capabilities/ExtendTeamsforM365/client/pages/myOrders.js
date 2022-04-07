73f30f548381729b979832d0edc97f20ef137db4
rabwill <rabiawilliams@gmail.com>
2022-04-08 03:13:14 +1000

    Added track order ac

--- src/extend-with-capabilities/ExtendTeamsforM365/client/pages/myOrders.js
+++ src/extend-with-capabilities/ExtendTeamsforM365/client/pages/myOrders.js
@@ -1,37 +1,79 @@
 import {
     getLoggedInEmployee
 } from '../identity/identityClient.js';
-
-async function displayUI() {
-
-    const displayElement = document.getElementById('content');
-    const ordersElement = document.getElementById('orders');
+async function displayUI() {   
     const messageDiv = document.getElementById('message');
-
     try {
         const employee = await getLoggedInEmployee();
-        if (employee) {
-            
-            displayElement.innerHTML = `
-                <h3>Orders for ${employee.displayName}<h3>
-            `;
-
-            employee.orders.forEach(order => {
-                const orderRow = document.createElement('tr');
-                orderRow.innerHTML = `<tr>
-                <td><a href="/pages/orderDetail.html?orderId=${order.orderId}">${order.orderId}</a></td>
-                <td>${(new Date(order.orderDate)).toDateString()}</td>
-                <td>${order.shipName}</td>
-                <td>${order.shipAddress}, ${order.shipCity} ${order.shipRegion || ''} ${order.shipPostalCode || ''} ${order.shipCountry}</td>
-            </tr>`;
-                ordersElement.append(orderRow);
-
-            });
+        if (employee) {            
+            displayAllMyOrders(employee);
+        }
+        if(microsoftTeams.app !== undefined) {
+            microsoftTeams.app.initialize();
+            microsoftTeams.app.getContext().then(context=> { 
+              if(context &&context.app.host.name==="Office" ){
+                displayMyRecentOrders(employee.orders);
+              }else if(context &&context.app.host.name==="Teams"){
+                const displayElement = document.getElementById('rOchart');
+                displayElement.style.display="flex";
+              }           
+            });           
         }
     }
     catch (error) {            // If here, we had some other error
         messageDiv.innerText = `Error: ${JSON.stringify(error)}`;
-    }
+    }   
 }
 
 displayUI();
+function displayAllMyOrders(employee) {
+    const ordersElement = document.getElementById('orders');
+    const displayElement = document.getElementById('content');
+    displayElement.innerHTML = `
+            <h3>Orders for ${employee.displayName}<h3>
+        `;
+
+    employee.orders.forEach(order => {
+        const orderRow = document.createElement('tr');
+        orderRow.innerHTML = `<tr>
+            <td><a href="/pages/orderDetail.html?orderId=${order.orderId}">${order.orderId}</a></td>
+            <td>${(new Date(order.orderDate)).toDateString()}</td>
+            <td>${order.shipName}</td>
+            <td>${order.shipAddress}, ${order.shipCity} ${order.shipRegion || ''} ${order.shipPostalCode || ''} ${order.shipCountry}</td>
+        </tr>`;
+        ordersElement.append(orderRow);
+
+    });
+}
+function displayMyRecentOrders(orders) {
+    const displayElement = document.getElementById('rOContent');
+    const rordersElement = document.getElementById('rOTable');
+    const rOFilesElement = document.getElementById('rOFiles');
+    const recentOrderArea = document.getElementById('rODiv');
+    recentOrderArea.style.display = "block";
+    displayElement.innerHTML = `
+    <h3>My recent orders<h3>
+`;
+    orders.splice(1, 5).forEach(order => {
+        const orderRow = document.createElement('tr');
+        orderRow.innerHTML = `<tr>
+    <td><a href="/pages/orderDetail.html?orderId=${order.orderId}">${order.orderId}</a></td>
+    <td>${(new Date(order.orderDate)).toDateString()}</td>
+    <td>${order.shipName}</td>
+    <td>${order.shipAddress}, ${order.shipCity} ${order.shipRegion || ''} ${order.shipPostalCode || ''} ${order.shipCountry}</td>
+</tr>`;
+        rordersElement.append(orderRow);
+    });
+    recentFiles.forEach(file => {
+        const orderRow = document.createElement('tr');
+        orderRow.innerHTML = `<tr>
+    <td><a href="/">${file.name}</a></td>             
+    <td>${file.modified}</td>
+          </tr>`;
+        rOFilesElement.append(orderRow);
+    });
+}
+const recentFiles=[{"name":"INV-order987","modified":"Today at 7.am"},
+{"name":"Invitation to partners","modified":"Yesterday at 4.pm"},
+{"name":"FAQ - Faulty deliveries","modified":"Yesterday at 2.pm"},
+{"name":"Customer survey order 9787","modified":"Yesterday at 11.am"}];
--- src/extend-with-capabilities/ExtendTeamsforM365/client/pages/orderDetail.js
+++ src/extend-with-capabilities/ExtendTeamsforM365/client/pages/orderDetail.js
@@ -12,9 +12,7 @@ async function displayUI() {
         const searchParams = new URLSearchParams(window.location.search);
         if (searchParams.has('orderId')) {
             const orderId = searchParams.get('orderId');
-
             const order = await getOrder(orderId);
-
             displayElement.innerHTML = `
                     <h1>Order ${order.orderId}</h1>
                     <p>Customer: ${order.customerName}<br />
@@ -23,7 +21,6 @@ async function displayUI() {
                     ${order.employeeTitle}: ${order.employeeName} (${order.employeeId})
                     </p>
                 `;
-
             order.details.forEach(item => {
                 const orderRow = document.createElement('tr');
                 orderRow.innerHTML = `<tr>
@@ -35,6 +32,29 @@ async function displayUI() {
                 detailsElement.append(orderRow);
 
             });
+            // microsoftTeams.app.initialize();
+            if(microsoftTeams.dialog.isSupported()){
+                alert(microsoftTeams.dialog.isSupported())
+                const btnTaskModuleElement = document.getElementById('btnTaskModule');
+                btnTaskModuleElement.style.display="block";
+                btnTaskModuleElement.addEventListener('click',  ev => {
+                    let submitHandler = (err, result) => {  };             
+                    var template = new ACData.Template(templatePayload); 
+                    // Expand the template with your `$root` data object.
+                    // This binds it to the data and produces the final Adaptive Card payload
+                    var cardPayload = template.expand({$root:{}});    
+                    const taskInfo = {
+                        card:cardPayload,
+                        title:"chat",
+                        height:310,
+                        width:430,
+                        url: null,               
+                        fallbackUrl: null,
+                        completionBotId: null,
+                    };  
+                    microsoftTeams.dialog.open(taskInfo, submitHandler);
+                });
+            }
 
         }
     }
@@ -42,6 +62,283 @@ async function displayUI() {
         message.innerText = `Error: ${JSON.stringify(error)}`;
     }
 }
-
+const templatePayload=`{
+    "type": "AdaptiveCard",
+    "body": [
+        {
+            "type": "ColumnSet",
+            "columns": [
+                {
+                    "type": "Column",
+                    "items": [
+                        {
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "spacing": "None",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "2:00 PM",
+                                            "wrap": true
+                                        }
+                                    ],
+                                    "width": "stretch"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "TextBlock",
+                            "spacing": "None",
+                            "text": "5 days ago",
+                            "isSubtle": true,
+                            "wrap": true
+                        }
+                    ],
+                    "width": "110px"
+                },
+                {
+                    "type": "Column",
+                    "backgroundImage": {
+                        "url": "https://messagecardplayground.azurewebsites.net/assets/SmallVerticalLineGray.png",
+                        "fillMode": "RepeatVertically",
+                        "horizontalAlignment": "Center"
+                    },
+                    "items": [
+                        {
+                            "type": "Image",
+                            "horizontalAlignment": "Center",
+                            "url": "https://messagecardplayground.azurewebsites.net/assets/CircleGreen_coffee.png",
+                            "altText": "Location A: Coffee"
+                        }
+                    ],
+                    "width": "auto",
+                    "spacing": "None"
+                },
+                {
+                    "type": "Column",
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "text": "**Order out for delivery**",
+                            "wrap": true
+                        },
+                        {
+                            "type": "ColumnSet",
+                            "spacing": "None",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "Image",
+                                            "url": "https://messagecardplayground.azurewebsites.net/assets/location_gray.png",
+                                            "altText": "Location"
+                                        }
+                                    ],
+                                    "width": "auto"
+                                },
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "Sydney",
+                                            "wrap": true
+                                        }
+                                    ],
+                                    "width": "stretch"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "ImageSet",
+                            "spacing": "Small",
+                            "imageSize": "Small",
+                            "images": [
+                                {
+                                    "type": "Image",
+                                    "url": "https://messagecardplayground.azurewebsites.net/assets/person_m1.png",
+                                    "size": "Small",
+                                    "altText": "Person with glasses and short hair"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "ColumnSet",
+                            "spacing": "Small",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "Delivery team",
+                                            "wrap": true
+                                        }
+                                    ],
+                                    "width": "stretch"
+                                }
+                            ]
+                        }
+                    ],
+                    "width": 40
+                }
+            ]
+        },
+        {
+            "type": "ColumnSet",
+            "spacing": "None",
+            "columns": [
+                {
+                    "type": "Column",
+                    "width": "110px"
+                },
+                {
+                    "type": "Column",
+                    "backgroundImage": {
+                        "url": "https://messagecardplayground.azurewebsites.net/assets/SmallVerticalLineGray.png",
+                        "fillMode": "RepeatVertically",
+                        "horizontalAlignment": "Center"
+                    },
+                    "items": [
+                        {
+                            "type": "Image",
+                            "horizontalAlignment": "Center",
+                            "url": "https://messagecardplayground.azurewebsites.net/assets/Gray_Dot.png"
+                        }
+                    ],
+                    "width": "auto",
+                    "spacing": "None"
+                },
+                {
+                    "type": "Column",
+                    "items": [
+                        {
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "Image",
+                                            "url": "https://messagecardplayground.azurewebsites.net/assets/car.png",
+                                            "altText": "Travel by car"
+                                        }
+                                    ],
+                                    "width": "auto"
+                                },
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "about 2 days ago",
+                                            "isSubtle": true,
+                                            "wrap": true
+                                        }
+                                    ],
+                                    "width": "stretch"
+                                }
+                            ]
+                        }
+                    ],
+                    "width": 40
+                }
+            ]
+        },
+        {
+            "type": "ColumnSet",
+            "spacing": "None",
+            "columns": [
+                {
+                    "type": "Column",
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "spacing": "None",
+                            "text": "8:00 PM",
+                            "wrap": true
+                        },
+                        {
+                            "type": "TextBlock",
+                            "spacing": "None",
+                            "text": "1 day ago",
+                            "isSubtle": true,
+                            "wrap": true
+                        }
+                    ],
+                    "width": "110px"
+                },
+                {
+                    "type": "Column",
+                    "backgroundImage": {
+                        "url": "https://messagecardplayground.azurewebsites.net/assets/SmallVerticalLineGray.png",
+                        "fillMode": "RepeatVertically",
+                        "horizontalAlignment": "Center"
+                    },
+                    "items": [
+                        {
+                            "type": "Image",
+                            "horizontalAlignment": "Center",
+                            "url": "https://messagecardplayground.azurewebsites.net/assets/CircleBlue_flight.png",
+                            "altText": "Location B: Flight"
+                        }
+                    ],
+                    "width": "auto",
+                    "spacing": "None"
+                },
+                {
+                    "type": "Column",
+                    "items": [
+                        {
+                            "type": "TextBlock",
+                            "text": "**Reached Brisbane warehouse**",
+                            "wrap": true
+                        },
+                        {
+                            "type": "ColumnSet",
+                            "spacing": "None",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "Image",
+                                            "url": "https://messagecardplayground.azurewebsites.net/assets/location_gray.png",
+                                            "altText": "Location"
+                                        }
+                                    ],
+                                    "width": "auto"
+                                },
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "400 George, Brisbane - 4000, QLD",
+                                            "wrap": true
+                                        }
+                                    ],
+                                    "width": "stretch"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "Image",
+                            "url": "https://messagecardplayground.azurewebsites.net/assets/SeaTacMap.png",
+                            "size": "Stretch",
+                            "altText": "Map of the Seattle-Tacoma airport"
+                        }
+                    ],
+                    "width": 40
+                }
+            ]
+        }
+    ],
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.5"
+}`
 
 displayUI();
