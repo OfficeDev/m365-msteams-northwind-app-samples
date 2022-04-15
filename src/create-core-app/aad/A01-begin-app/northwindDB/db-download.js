5c7cfae2c413b038a025919de6dc4d3645d94f5b
Bob German <bgerman1@hotmail.com>
2022-04-15 11:30:47 -0400

    Cleaned up download script

--- src/create-core-app/aad/A01-begin-app/northwindDB/db-download.js
+++ src/create-core-app/aad/A01-begin-app/northwindDB/db-download.js
@@ -1,5 +1,4 @@
 import fetch from 'node-fetch';
-import fs from 'fs';
 import { join, dirname } from 'path';
 import { Low, JSONFile } from 'lowdb';
 import { fileURLToPath } from 'url';
@@ -8,41 +7,45 @@ import { fileURLToPath } from 'url';
 const ODATA_SERVICE = "https://services.odata.org/V4/Northwind/Northwind.svc";
 const TABLE_NAMES = ["Categories", "Customers", "Employees", "Order_Details", "Orders",
                      "Products", "Regions", "Suppliers", "Territories"];
-
-// Get directory for json files
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// JSON files live in the same folder as this script
+const directoryPath = dirname(fileURLToPath(import.meta.url));
 
 // Each table gets its own JSON "database" to limit the need to write the whole
 // database every time a table changes
 for (let tableName of TABLE_NAMES) {
 
     // Set up the JSON db
-    const file = join(__dirname, `${tableName}.json`);
+    const file = join(directoryPath, `${tableName}.json`);
     const adapter = new JSONFile(file);
     const db = new Low(adapter);
 
     // Clear the data, copy the table, and write the result
-    db.data = { };
+    db.data = {};
     await copyTable(db, tableName);
     await db.write();
 
 }
 
-// Copy a table
+// Copy a table, iterating through pages of data as needed
 async function copyTable(db, tableName) {
 
     if (!db.data[tableName]) db.data[tableName] = [];
 
-    // Fetch first batch of rows from OData
+    // Fetch first page of rows from OData
+    let pageCount = 1;
     let rowCount, nextLink;
     [rowCount, nextLink] = await copyRows(db, `${ODATA_SERVICE}/${tableName}?$count=true`, tableName);
 
     // If there are more rows, read them
     while (nextLink) {
         [rowCount, nextLink] = await copyRows(db, `${ODATA_SERVICE}/${nextLink}`, tableName);
+        pageCount++;
     }
 
-    console.log(`Wrote ${rowCount} rows of ${tableName} for a total of ${db.data[tableName].length}`);
+    if (rowCount !== db.data[tableName].length) {
+        console.log(`ERROR: Missing rows in ${tableName}`);
+    }
+    console.log(`Created ${tableName}.json with ${db.data[tableName].length} rows (${pageCount} page${pageCount>1?'s':''} downloaded)`);
 }
 
 // Copy a row
