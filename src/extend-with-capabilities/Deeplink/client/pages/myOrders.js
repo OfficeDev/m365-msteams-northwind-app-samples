39353541eed8303208219abdd4195519a90b888f
Bob German <bgerman1@hotmail.com>
2022-05-31 15:45:59 -0400

    Deep link lab works but should use SDK to generate

--- src/extend-with-capabilities/Deeplink/client/pages/myOrders.js
+++ src/extend-with-capabilities/Deeplink/client/pages/myOrders.js
@@ -2,7 +2,7 @@ import {
     getLoggedInEmployee
 } from '../identity/identityClient.js';
 import { inTeams } from '../modules/teamsHelpers.js';
-import 'https://statics.teams.cdn.office.net/sdk/v1.11.0/js/MicrosoftTeams.min.js';
+import 'https://res.cdn.office.net/teams-js/2.0.0/js/MicrosoftTeams.min.js';
 async function displayUI() {
 
     const displayElement = document.getElementById('content');
@@ -14,35 +14,38 @@ async function displayUI() {
         // Handle incoming deep links by redirecting to the selected order
         if (await inTeams()) {
 
-            microsoftTeams.initialize(async () => {
-                microsoftTeams.getContext(async (context) => {
-                    if (context.subEntityId) {
-                        window.location.href = `/pages/orderDetail.html?orderId=${context.subEntityId}`;
-                    }
-                });
-            });
-        }
+            await microsoftTeams.app.initialize();
+            let context = await microsoftTeams.app.getContext();
 
-        // Display order data
-        const employee = await getLoggedInEmployee();
-        if (employee) {
-            displayElement.innerHTML = `<h3>Orders for ${employee.displayName}<h3>`;
-            employee.orders.forEach(order => {
-                const orderRow = document.createElement('tr');
-                orderRow.innerHTML = `<tr>
-                <td><a href="/pages/orderDetail.html?orderId=${order.orderId}">${order.orderId}</a></td>
-                <td>${(new Date(order.orderDate)).toDateString()}</td>
-                <td>${order.shipName}</td>
-                <td>${order.shipAddress}, ${order.shipCity} ${order.shipRegion || ''} ${order.shipPostalCode || ''} ${order.shipCountry}</td>
-                </tr>`;
-                ordersElement.append(orderRow);
-            });
+            if (context.subEntityId) {
+                window.location.href = `/pages/orderDetail.html?orderId=${context.subEntityId}`;
+            }
+
+            // Display order data
+            const employee = await getLoggedInEmployee();
+            if (employee) {
+
+                displayElement.innerHTML = `
+                <h3>Orders for ${employee.displayName}<h3>
+            `;
+
+                employee.orders.forEach(order => {
+                    const orderRow = document.createElement('tr');
+                    orderRow.innerHTML = `<tr>
+                        <td><a href="/pages/orderDetail.html?orderId=${order.orderId}">${order.orderId}</a></td>
+                        <td>${(new Date(order.orderDate)).toDateString()}</td>
+                        <td>${order.shipName}</td>
+                        <td>${order.shipAddress}, ${order.shipCity} ${order.shipRegion || ''} ${order.shipPostalCode || ''} ${order.shipCountry}</td>
+                    </tr>`;
+                    ordersElement.append(orderRow);
+
+                });
+            }
         }
     }
     catch (error) {            // If here, we had some other error
         messageDiv.innerText = `Error: ${JSON.stringify(error)}`;
     }
-
 }
 
 displayUI();
